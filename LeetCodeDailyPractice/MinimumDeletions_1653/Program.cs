458f63573eae9d3728ae4fec9e83d660b1b277a4
杨士航 <yangsh@genepoint.cn>
2023-03-06 16:08:46 +0800

    update

--- LeetCodeDailyPractice/MinimumDeletions_1653/Program.cs
+++ LeetCodeDailyPractice/MinimumDeletions_1653/Program.cs
@@ -69,5 +69,41 @@
             }
             return res;
         }
+
+        /// <summary>
+        /// 动态规划（一次遍历）
+        /// 
+        /// 考虑 s 的最后一个字母：
+        /// 
+        /// 如果它是 'b'，则无需删除，问题规模缩小，变成「使 s 的前 n−1 个字母平衡的最少删除次数」。
+        /// 如果它是 'a'：
+        ///     删除它，则答案为「使 s 的前 n−1 个字母平衡的最少删除次数」加上 1
+        ///     保留它，那么前面的所有 'b' 都要删除
+        /// 	
+        /// 设 cntB 为前 i 个字母中 'b' 的个数。定义 f[i] 表示使 s 的前 i 个字母平衡的最少删除次数：
+        /// 
+        /// 如果第 i 个字母是 'b'，则 f[i]=f[i−1]；
+        /// 如果第 i 个字母是 'a'，则 f[i]=min⁡(f[i−1]+1,cntB)。
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int MinimumDeletions_Pro2(string s)
+        {
+            int f = 0;
+            int countB = 0;
+            foreach (var item in s)
+            {
+                if (item.Equals('b'))
+                {
+                    countB++;
+                }
+                else
+                {
+                    f = Math.Min(f + 1, countB);
+                }
+            }
+
+            return f;
+        }
     }
 }
